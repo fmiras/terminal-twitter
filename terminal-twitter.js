f478ec6f14e8ce694671bcd1b371d2851b64221d
Federico Miras <miras.federico@gmail.com>
2017-07-12 03:51:52 -0300

    Rename webtask to terminal-twitter

---
+++ terminal-twitter.js
@@ -0,0 +1,22 @@
+const box = require('cli-box')
+const colors = require('colors')
+
+module.exports = function (ctx, done) {
+
+  //Make mentions and hashtags colorfouls
+  const tweet = ctx.data.tweet
+    .replace(/(#[a-z0-9][a-z0-9\-_]*)/ig, colors.cyan('$1'))
+    .replace(/(@[a-z0-9][a-z0-9\-_]*)/ig, colors.cyan('$1'))
+  const username = colors.cyan(`@${ctx.data.username}`)
+  const item = `${username}\n\n${tweet}`
+
+  const terminalTweet = box('0x0', {
+    text: item,
+    stretch: true,
+    autoEOL: true,
+    hAlign: 'left'
+  });
+
+  console.log(terminalTweet);
+  done(null, 'Check the logs with wt logs command!')
+}
