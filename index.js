de1ee1dfadf66837e881f9167332bc5c20943c34
Federico Miras <miras.federico@gmail.com>
2018-01-25 16:15:25 -0300

    Change usage of req query to json body parsing of micro

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const box = require('cli-box')
 const colors = require('colors')
-const url = require('url')
+const { json } = require('micro')
 
 module.exports = async (req) => {
-  const query = url.parse(req.url, true).query
+  const data = await json(req)
 
   // Make mentions and hashtags colorfouls
-  const tweet = query.tweet
+  const tweet = data.tweet
     .replace(/(#[a-z0-9][a-z0-9\-_]*)/ig, colors.cyan('$1'))
     .replace(/(@[a-z0-9][a-z0-9\-_]*)/ig, colors.cyan('$1'))
-  const username = colors.cyan(`@${query.username}`)
+  const username = colors.cyan(`@${data.username}`)
   const item = `${username}\n\n${tweet}`
 
   const terminalTweet = box('0x0', {
